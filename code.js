1fcfc7a7abd188a9ffd389577888900cd3340097
M00ose <tyrone.jacalan@protonmail.com>
2022-09-15 20:43:51 +0800

    Added functionality to calculate a winner

--- code.js
+++ code.js
@@ -13,9 +13,19 @@ var game = (() => {
     square.addEventListener("click", function () {
       active = square;
       doMove(active);
-      mapBoard();
+      calculateBoard();
     })
   );
+
+  const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+  ];
 
   function getSign() {
     let sign;
@@ -34,19 +44,50 @@ var game = (() => {
     gameOver();
   }
 
+  function calculateBoard() {
+    let board = [];
+    squares.forEach((square) => board.push(square.innerHTML));
+
+    winningConditions.forEach((condition) => {
+      let first = condition[0];
+      let second = condition[1];
+      let third = condition[2];
+      let sign = board[first];
+
+      console.log(board);
+      //console.log([board[first], board[second], board[third]]);
+
+      return board[first] != "" &&
+        board[second] != "" &&
+        board[third] != "" &&
+        board[first] === board[second] &&
+        board[first] === board[third]
+        ? declareWinner(sign)
+        : false;
+    });
+  }
+
+  function declareWinner(playerSign) {
+    let winner;
+    players.forEach((player) => {
+      player.getAttribute("value") === playerSign
+        ? (winner = player.innerHTML)
+        : false;
+      console.log([playerSign, player.getAttribute("value"), player.innerHTML]);
+    });
+    console.log(winner);
+    clearBoard();
+  }
+
   function gameOver() {
     return rounds === 8 ? clearBoard() : rounds++;
   }
 
   function clearBoard() {
-    squares.forEach((square) => (square.innerHTML = " "));
+    squares.forEach((square) => (square.innerHTML = ""));
     rounds = 0;
     totalRounds++;
-    console.log(totalRounds);
+    // console.log(totalRounds);
     roundTab.innerHTML = `Round ${totalRounds}`;
   }
-
-  return {
-    doMove: doMove,
-  };
 })();
